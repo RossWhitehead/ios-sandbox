78ea89907a6d9a9b7969709bb1d0a27c72c1a552
Ross Whitehead <ross.whitehead@outlook.com>
2018-07-02 19:01:57 +0100

    Initial Commit

---
+++ ios-sandbox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-sandbox
+//
+//  Created by Ross Whitehead on 02/07/2018.
+//  Copyright © 2018 Ross Whitehead. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
